a9f03c1c2c7afdc69e54c0f7dfe27f04cb028301
advico <adriawndev@gmail.com>
2024-12-15 12:50:18 -0300

    atualizar classe

--- desafioDIO/src/ContaInvestimento.java
+++ desafioDIO/src/ContaInvestimento.java
@@ -1,3 +1,10 @@
-public class ContaInvestimento {
+public class ContaInvestimento extends Conta {
+    public ContaInvestimento(double saldoInicial) {
+        super("Investimento", saldoInicial);
+    }
 
+    @Override
+    public double calculaValorTarifaManutencao() {
+        return 10.0; // Tarifa fixa para contas investimento
+    }
 }
